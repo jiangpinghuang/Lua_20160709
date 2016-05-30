2737334b330e60fb868c5ea1204b0e19e4a182ae
hjpwhu <hjpwhu@qq.com>
2016-05-30 14:28:42 +0800

    ini

--- src/hjp.mac.torch.basis.demo/nn.lua
+++ src/hjp.mac.torch.basis.demo/nn.lua
@@ -1,8 +1,6 @@
---[[
-Containers.
---]]
-
 require 'nn'
+--[[
+--||Containers.||--
 
 -- Sequential. --
 mlp = nn.Sequential()
@@ -76,3 +74,86 @@ mlp:add(nn.SpatialConvolutionMM(inputSize, outputSize, 1, 1))
 mlp:add(nn.SpatialConvolutionMM(inputSize, outputSize, 3, 3))
 mlp:add(nn.SpatialConvolutionMM(inputSize, outputSize, 4, 4))
 print(mlp:forward(input))
+--]]
+
+
+--//////////////////////////////////////////--
+--[[
+--||Overview.||--
+--]]
+-- A simple neural network (perceptron) with 10 inputs. --
+-- mlp = nn.Linear(10, 1)
+
+-- Multi-layer perceptron. --
+mlp = nn.Sequential()
+mlp:add(nn.Linear(10, 25))
+mlp:add(nn.Tanh())
+mlp:add(nn.Linear(25, 1))
+
+print(mlp)
+
+criterion = nn.MSECriterion()
+trainer = nn.StochasticGradient(mlp, criterion)
+-- No dataset. --
+--trainer:train(dataset)
+
+function gradUpdate(mlp, x, y, criterion, learningRate) 
+  local pred = mlp:forward(x)
+  local err = criterion:forward(pred, y)
+  local gradCriterion = criterion:backward(pred, y)
+  mlp:zeroGradParameters()
+  mlp:backward(x, gradCriterion)
+  mlp:updateParameters(learningRate)
+end
+
+-- our optimization procedure will iterate over the modules, so only share
+-- the parameters
+mlp = nn.Sequential()
+linear = nn.Linear(2,2)
+linear_clone = linear:clone('weight','bias') -- clone sharing the parameters
+mlp:add(linear)
+mlp:add(linear_clone)
+function gradUpdate(mlp, x, y, criterion, learningRate) 
+  local pred = mlp:forward(x)
+  local err = criterion:forward(pred, y)
+  local gradCriterion = criterion:backward(pred, y)
+  mlp:zeroGradParameters()
+  mlp:backward(x, gradCriterion)
+  mlp:updateParameters(learningRate)
+end
+
+-- our optimization procedure will use all the parameters at once, because
+-- it requires the flattened parameters and gradParameters Tensors. Thus,
+-- we need to share both the parameters and the gradParameters
+mlp = nn.Sequential()
+linear = nn.Linear(2,2)
+-- need to share the parameters and the gradParameters as well
+linear_clone = linear:clone('weight','bias','gradWeight','gradBias')
+mlp:add(linear)
+mlp:add(linear_clone)
+params, gradParams = mlp:getParameters()
+function gradUpdate(mlp, x, y, criterion, learningRate, params, gradParams)
+  local pred = mlp:forward(x)
+  local err = criterion:forward(pred, y)
+  local gradCriterion = criterion:backward(pred, y)
+  mlp:zeroGradParameters()
+  mlp:backward(x, gradCriterion)
+  -- adds the gradients to all the parameters at once
+  params:add(-learningRate, gradParams)
+end
+
+
+--//////////////////////////////////////////--
+--[[
+--||Transfer Function Layers.||--
+--]]
+
+require 'gnuplot'
+-- HardTanh. --
+ii = torch.linspace(-2, 2)
+m = nn.HardTanh()
+oo = m:forward(ii)
+go = torch.ones(100)
+gi = m:backward(ii, go)
+gnuplot.plot({'f(x)', ii, oo, '+-'}, {'df/dx', ii, gi, '+-'})
+gnuplot.grid(true)
