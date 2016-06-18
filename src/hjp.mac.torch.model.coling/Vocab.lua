62c7c12f29ceaed784394d4977b7041f7cadb0ac
hjp <hjp@whu>
2016-06-18 11:17:00 +0800

    ini

--- src/hjp.mac.torch.model.coling/Vocab.lua
+++ src/hjp.mac.torch.model.coling/Vocab.lua
@@ -1,11 +1,8 @@
 --[[
-We Thank Kai Sheng Tai for providing the preprocessing codes. 
-
 A vocabulary object. Initialized from a file with one vocabulary token per line.
 Maps between vocabulary tokens and indices. If an UNK token is defined in the
 vocabulary, returns the index to this token if queried for an out-of-vocabulary
 token.
-
 --]]
 
 local Vocab = torch.class('similarityMeasure.Vocab')
