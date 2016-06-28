2c5be2c17516bf2bd15d3ab7d2d854527b1c7b45
hjpwhu <hjpwhu@qq.com>
2016-06-28 21:46:25 +0800

    ini

--- src/hjp.mac.torch.basis.demo/cnn_demo.lua
+++ src/hjp.mac.torch.basis.demo/cnn_demo.lua
@@ -1,15 +1,20 @@
 require 'torch'
 require 'nn'
 
-input = torch.randn(4, 5)
+input = torch.randn(4, 3)
 print(input)
 
-local is, os, kw, dw = 5, 1, 1, 1
+local is, os, kw, dw = 3, 3, 1, 1
 
-cnn = nn.TemporalConvolution(is, os, kw, dw)
+cnn = nn.Sequential()
+cnn:add(nn.TemporalConvolution(is, os, kw, dw))
+cnn:add(nn.ReLU())
+--cnn = nn.TemporalConvolution(is, os, kw, dw)
 
-weights = torch.reshape(cnn.weight, is) 
-print(weights)
-
+print(cnn.weight)
+print(cnn.bias)
+--weights = torch.reshape(cnn.weight, is) 
+--print(weights)
+print('output:')
 output = cnn:forward(input)
 print(output)
